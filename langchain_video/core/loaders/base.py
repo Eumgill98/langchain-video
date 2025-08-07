305d7e6da2b12825052214277a908747dd34bdcb
Eumgill98 <wjdghcks02@gmail.com>
2025-08-07 23:24:57 +0900

    feat: add helper methods in abstact base class for multimadal blob loader
    - add helper methos that convert paths (list of str or Path) to List[Path] types

--- langchain_video/core/loaders/base.py
+++ langchain_video/core/loaders/base.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
-from typing import Union
+from typing import Union, List
+from pathlib import Path
 
-from langchain_video.core.blobs import VideoBlob, ImageBlob, AudioBlob
+from langchain_video.core.blobs import VideoBlob, ImageBlob, AudioBlob, PathLike
 
 if TYPE_CHECKING:
     from collections.abc import Iterable
@@ -16,6 +17,14 @@ class BaseMultiModalBlobLoader(ABC):
     Implementer should be able to load raw content from a storage system according
     to some criteria and return the raw content lazily as a stream of Multimodal blobs.
     """
+    
+    def _convert2paths(
+        self,
+        paths: Union[PathLike, List[Union[str, Path]]],
+    ) -> List[Path]:
+        """Convert input path(s) to a list of pathlib.Path objects."""
+        return [p if isinstance[p, Path] else Path(p) for p in paths]
+
     @abstractmethod
     def yield_blobs(
         self,
