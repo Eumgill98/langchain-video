76b821d857cfa5de7cecfe01c000a7a6baf0ca55
wjdghcks6735 <wjdghcks02@gmail.com>
2025-08-19 15:16:15 +0900

    feat: add package `langchain_video` library

---
+++ langchain_video/__init__.py
@@ -0,0 +1,2 @@
+from .core import *
+from .embeddings import *
---
+++ langchain_video/core/__init__.py
@@ -0,0 +1,6 @@
+from .blobs import *
+from .embeddings import *
+from .loaders import *
+from .resamplers import *
+from .spliters import *
+from .vector_stores import *
